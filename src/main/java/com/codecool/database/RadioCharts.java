3fa7a3044117691da841e63df846643983e9461a
dezur <petrenyi@gmail.com>
2021-03-24 17:31:19 +0100

    Finish

--- src/main/java/com/codecool/database/RadioCharts.java
+++ src/main/java/com/codecool/database/RadioCharts.java
@@ -1,6 +1,39 @@
 package com.codecool.database;
 
+import java.sql.*;
 
 public class RadioCharts {
-    //TODO: implement this class
+
+    private final String url;
+    private final String user;
+    private final String password;
+
+    public RadioCharts(String url, String user, String password) {
+        this.url = url;
+        this.user = user;
+        this.password = password;
+    }
+
+    public String getMostPlayedSong() {
+        String SQL = "SELECT song FROM music_broadcast GROUP BY song ORDER BY SUM(times_aired) DESC, artist ASC";
+        return createConnection(SQL);
+    }
+
+    public String getMostActiveArtist() {
+        String SQL = "SELECT artist, COUNT(DISTINCT song) as songs FROM music_broadcast GROUP BY artist ORDER BY songs DESC";
+        return createConnection(SQL);
+    }
+
+    private String createConnection(String query) {
+        try {
+            Connection conn = DriverManager.getConnection(url, user, password);
+            Statement statement = conn.createStatement();
+            ResultSet res = statement.executeQuery(query);
+            res.next();
+            return res.getString(1);
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return "";
+    }
 }
